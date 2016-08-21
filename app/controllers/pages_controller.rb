c92cadf19acc77a4a6767c2b83bbdbe2998751f1
Nathan Disidore <ndisidore@gmail.com>
2016-08-21 13:52:06 -0400

    Add entry code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,10 @@ class PagesController < ApplicationController
   layout 'site_main'
 
   def home
-    render layout: "site_main"
+    render layout: 'site_main'
+  end
+
+  def entry
+    render layout: 'entry'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
 
+  get 'entry' => 'pages#entry'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
